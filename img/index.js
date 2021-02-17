fe8408186840d0a63d5de2813253abe8a963f4f6
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2021-02-17 12:07:30 -0500

    lunch-break

--- img/index.js
+++ img/index.js
@@ -1,7 +1,7 @@
 const url = 'http://localhost:3000/dishes'
 const picture = document.querySelector('.detail-image')
 const name = document.querySelector('.name')
-const updateForm = document.querySelector('#update-form')
+const addForm = document.querySelector('#add-form')
 // const deleteButton = document.querySelector('.button')
 const dishDiv = document.querySelector('.dishes')
 
@@ -22,6 +22,7 @@ function displayAllDishes(dishObject){
             const dishImage = document.createElement('img')
             const dishName = document.createElement('p')
             const deleteButton = document.createElement('button')
+            const editButton = document.createElement('button')
 
             dishImage.src = dish.image
 
@@ -31,21 +32,27 @@ function displayAllDishes(dishObject){
             dishCard.dataset.id = dish.id
 
 
-            deleteButton.innerText = "delete"
+            editButton.innerText = "edit"
+            editButton.className = "edit-button"
+            editButton.id = "edit-one-dish"
+            editButton.addEventListener('click', editOneDish )
+
+
             deleteButton.className = "button"
+            deleteButton.innerText = "delete"
             deleteButton.id = "delete-one-dish"
             deleteButton.addEventListener('click', deleteOneDish )
             
 
             
-            dishCard.append(dishImage, dishName, deleteButton)
+            dishCard.append(dishImage, dishName, deleteButton,editButton)
             // dishCard.innerHTML += `<button class="button" id="delete-oneDish">Delete</button>`
             dishDiv.append(dishCard)
         })
        
 }
 
-    updateForm.addEventListener('submit', createDish)
+    addForm.addEventListener('submit', createDish)
     
     function createDish(e){
         e.preventDefault()
@@ -56,7 +63,7 @@ function displayAllDishes(dishObject){
         const newDishHistory = document.querySelector('#dish-history').value
         const newDishIngredients = document.querySelector('#dish-ingredients').value
         const newDishInstructions = document.querySelector('#dish-instructions').value
-    
+        
         const newDish = {name: newDishName, image: newDishImage, category: newDishCategory, food_history: newDishHistory, ingredients: newDishIngredients, instructions: newDishInstructions}
         
         fetch(url,{
@@ -65,7 +72,9 @@ function displayAllDishes(dishObject){
                 'Content-Type': 'application/json'
             }, body: JSON.stringify(newDish) 
         })  .then(res => res.json())
-            .then(displayNewDish) 
+        .then(displayNewDish) 
+        
+        e.target.reset()
     }
 
 
@@ -74,6 +83,7 @@ function displayAllDishes(dishObject){
             const dishImage = document.createElement('img')
             const dishName = document.createElement('p')
             const deleteButton = document.createElement('button')
+            const updateButton = document.createElement('button')
 
             dishImage.src = dish.image
 
@@ -87,10 +97,15 @@ function displayAllDishes(dishObject){
             deleteButton.className = "button"
             deleteButton.id = "delete-one-dish"
             deleteButton.addEventListener('click', deleteOneDish )
+
+            updateButton.innerText = "update"
+            updateButton.className = "update-button"
+            updateButton.id = "update-one-dish"
+            updateButton.addEventListener('click', updateOneDish )
             
 
             
-            dishCard.append(dishImage, dishName, deleteButton)
+            dishCard.append(dishImage, dishName, deleteButton, updateButton)
             dishDiv.append(dishCard)
     }
 
@@ -120,6 +135,34 @@ function displayAllDishes(dishObject){
     }
     
 
-getAllDishes()
+    
+    
+    function editOneDish(e){
+        
+        e.preventDefault()
+        const updatedDishName = document.querySelector('#edit-dish-name').value
+        const updatedDishImage = document.querySelector('#edit-dish-image').value
+        const updatedDishIngredients = document.querySelector('#edit-dish-ingredients').value
+        
+        // const editForm = document.querySelector('#edit-form')
+        // console.log(editForm)
+        
+        let dishId = e.target.parentNode.attributes["data-id"].value
+        
+        const updatedDish = {name: updatedDishName, image: updatedDishImage, ingredients: updatedDishIngredients}
+        
+        fetch(`${url}/${dishId}`,{
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            }, body: JSON.stringify(updatedDish) 
+        })  .then(res => res.json())
+        .then(updatedDish => console.log(updatedDish)) 
+        
+    }
 
 
+
+
+
+    getAllDishes()
